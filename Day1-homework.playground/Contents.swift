5039a71583800b52c8d01f253395ae4fe31170c6
Lacey Vu <laceylin2010@gmail.com>
2016-02-01 15:42:22 -0800

    finishing the first homework assignment for day 1

---
+++ Day1-homework.playground/Contents.swift
@@ -0,0 +1,52 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Question 1
+
+func cityLocation(location: String) -> String {
+    let city = "I live in " + location
+    let number = Int(location)
+    return city
+}
+
+print(cityLocation("Seattle"))
+
+
+
+//Question 2
+
+let tuple: (color: String, number: Int, siblings: Bool) = ("Blue", 10, true)
+let message = "My favorite color is \(tuple.color)"
+
+//string interpolation to turn it into one string
+
+
+//Question 3
+
+func printAndCount(stringToPrint: String) -> Int{
+    print(stringToPrint)
+    return stringToPrint.characters.count
+}
+func printWithoutCounting(stringToPrint: String){
+    printAndCount(stringToPrint)
+}
+printAndCount("I like Apples")
+
+
+//Question 4
+
+var fruit = "Oranges"
+var reverseFruit = ""
+
+for character in fruit.characters {
+    
+    //had to use type conversion to turn character (which is not a string) into a string
+    
+    reverseFruit = String(character) + reverseFruit
+}
+
+print(reverseFruit)
+
+
+
